57cbae82df174abb1c26493a4ff9c148e924e567
Rayan Wilbert <rayan.rmw@gmail.com>
2022-11-03 15:40:02 -0300

    Add github OAuth request

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,4 +1,17 @@
-import express from 'express';
-const server = express();
+import 'dotenv/config';
 
-server.listen(3333, () => console.log('Go to http://localhost:3333/'));
+import express from 'express';
+const app = express();
+
+app.get('/github', (request, response) =>
+  response.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`
+  )
+);
+
+app.get('/signin/callback', (request, response) => {
+  const { code } = request.query;
+  return response.json(code);
+});
+
+app.listen(3333, () => console.log('Go to http://localhost:3333/'));
