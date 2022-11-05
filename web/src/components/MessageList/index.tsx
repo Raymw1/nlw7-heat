1ac33e85fc5fd6c73a013848f05bec095300ede9
Rayan Wilbert <rayan.rmw@gmail.com>
2022-11-05 17:02:15 -0300

    Add MessageList component structure

--- web/src/components/MessageList/index.tsx
+++ web/src/components/MessageList/index.tsx
@@ -1,7 +1,50 @@
 import styles from './styles.module.scss';
 
+import logoImg from '../../assets/logo.svg';
+
 export function MessageList() {
   return (
-    <h1>MessageList</h1>
+    <div className={styles.messageListWrapper}>
+      <img src={logoImg} alt='DoWhile 2021' />
+      <ul className={styles.messageList}>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint
+            cillum sint consectetur cupidatat.
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src='https://github.com/Raymw1.png' alt='Rayan Wilbert' />
+            </div>
+            <span>Rayan Wilbert</span>
+          </div>
+        </li>
+
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint
+            cillum sint consectetur cupidatat.
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src='https://github.com/Raymw1.png' alt='Rayan Wilbert' />
+            </div>
+            <span>Rayan Wilbert</span>
+          </div>
+        </li>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint
+            cillum sint consectetur cupidatat.
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src='https://github.com/Raymw1.png' alt='Rayan Wilbert' />
+            </div>
+            <span>Rayan Wilbert</span>
+          </div>
+        </li>
+      </ul>
+    </div>
   );
 }
