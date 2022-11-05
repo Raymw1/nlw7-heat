5540c00b08a2804cb293b3f636ee18252d5e366f
Rayan Wilbert <rayan.rmw@gmail.com>
2022-11-04 23:23:39 -0300

    Add webSocket

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,21 @@
 import 'dotenv/config';
 import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+import cors from 'cors';
 import { routes } from './routes';
 
 const app = express();
+app.use(cors());
+
+const serverHttp = http.createServer(app);
+const io = new Server(serverHttp, { cors: { origin: '*' } });
+
+io.on('connection', (socket) => {
+  console.log(socket.id);
+});
 
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => console.log('Go to http://localhost:3333/'));
+export { serverHttp, io };
--- server/src/controllers/CreateMessageController.ts
+++ server/src/controllers/CreateMessageController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import { io } from '../app';
 import { CreateMessageService } from '../services/CreateMessageService';
 
 class CreateMessageController {
@@ -6,6 +7,13 @@ class CreateMessageController {
     const { text } = request.body;
     const createMessageService = new CreateMessageService();
     const message = await createMessageService.execute(text, request.user_id);
+    const infoWS = {
+      text: message.text,
+      user_id: message.user_id,
+      created_at: message.created_at,
+      user: { name: message.user.name, avatar_url: message.user.avatar_url },
+    };
+    io.emit('new_message', infoWS);
     return response.status(201).json(message);
   }
 }
