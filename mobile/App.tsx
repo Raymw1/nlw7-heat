ba8c36b9af67aacb059afd48c447338dbea6aeed
Rayan Wilbert <rayan.rmw@gmail.com>
2022-11-06 16:34:25 -0300

    Add external fonts

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,7 +1,26 @@
+import { useCallback } from 'react';
 import { Home } from './src/screens/Home';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import * as SplashScreen from 'expo-splash-screen';
+import { View } from 'react-native';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [fontsLoaded] = useFonts([Roboto_400Regular, Roboto_700Bold]);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
+
+
   return (
-    <Home />
+    <View onLayout={onLayoutRootView} style={{ flex: 1 }}>
+      <Home />
+    </View>
   );
 }
