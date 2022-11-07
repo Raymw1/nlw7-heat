82c4d4b3d861c34c82c20283c329d6fc9f2a1cb2
Rayan Wilbert <rayan.rmw@gmail.com>
2022-11-06 21:21:44 -0300

    Add Moti animation in Message component

--- mobile/src/components/Message/index.tsx
+++ mobile/src/components/Message/index.tsx
@@ -1,4 +1,5 @@
 import { View, Text } from 'react-native';
+import { MotiView } from 'moti';
 import { UserPhoto } from '../UserPhoto';
 
 import { styles } from './styles';
@@ -18,12 +19,17 @@ type MessageProps = {
 
 export function Message({ data }: MessageProps) {
   return (
-    <View style={styles.container}>
+    <MotiView
+      style={styles.container}
+      from={{ opacity: 0, translateY: -50 }}
+      animate={{ opacity: 1, translateY: 0 }}
+      transition={{ type: 'timing', duration: 700 }}
+    >
       <Text style={styles.message}>{data.text}</Text>
       <View style={styles.footer}>
         <UserPhoto size='small' imageUri={data.user.avatar_url} />
         <Text style={styles.userName}>{data.user.name}</Text>
       </View>
-    </View>
+    </MotiView>
   );
 }
